7db3a20b67d0b3e4a278ee2edbbb07ad882db940
yeonsubak <yeonsubak@gmail.com>
2022-09-21 23:17:27 +0900

    Modify ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,22 @@
-module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
-};
+module.export = ({ env }) => ({
+  apps: [
+    {
+      name: "staysane-blog-strapi",
+      cwd: env("CWD"),
+      script: "yarn",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+        DATABASE_HOST: env("DATABASE_HOST"),
+        DATABASE_PORT: env("DATABASE_PORT"),
+        DATABASE_NAME: env("DATABASE_NAME"),
+        DATABASE_USERNAME: env("DATABASE_USERNAME"),
+        DATABASE_PASSWORD: env("DATABASE_PASSWORD"),
+        AWS_ACCESS_KEY_ID: env("AWS_ACCESS_KEY_ID"),
+        AWS_ACCESS_SECRET: env("AWS_ACCESS_SECRET"),
+        AWS_REGION: env("AWS_REGION"),
+        AWS_BUCKET_NAME: env("AWS_BUCKET_NAME"),
+      },
+    },
+  ],
+});
