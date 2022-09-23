c7740c9dff31584097e86ee6ef0522d3156188dc
yeonsubak <yeonsubak@gmail.com>
2022-09-23 23:05:25 +0900

    Fix AWS S3 provider configuration in middleware.ts

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -11,4 +11,30 @@ exports.default = [
     'strapi::session',
     'strapi::favicon',
     'strapi::public',
+    {
+        name: 'strapi::security',
+        config: {
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    'connect-src': ["'self'", 'https:'],
+                    'img-src': [
+                        "'self'",
+                        'data:',
+                        'blob:',
+                        'dl.airtable.com',
+                        'staysane-blog-storage.s3.ap-northeast-2.amazonaws.com',
+                    ],
+                    'media-src': [
+                        "'self'",
+                        'data:',
+                        'blob:',
+                        'dl.airtable.com',
+                        'staysane-blog-storage.s3.ap-northeast-2.amazonaws.com',
+                    ],
+                    upgradeInsecureRequests: null,
+                },
+            },
+        },
+    },
 ];
