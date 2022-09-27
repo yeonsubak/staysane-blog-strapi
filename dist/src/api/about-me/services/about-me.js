f7a98c2cb64f8f951871a7d54eee2add91dbe575
yeonsubak <yeonsubak@gmail.com>
2022-09-27 19:12:46 +0900

    Add content type: AboutMe

---
+++ dist/src/api/about-me/services/about-me.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * about-me service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::about-me.about-me');
