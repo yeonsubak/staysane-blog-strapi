0c5426074635e35e4b7628b8afa4c0bfd5a0a6b2
Yeonsu Bak <91791222+yeonsubak@users.noreply.github.com>
2022-10-13 23:21:17 +0900

    Delete dist directory

--- dist/src/api/about-me/controllers/about-me.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * about-me controller
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::about-me.about-me');
--- dist/src/api/about-me/services/about-me.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * about-me service
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::about-me.about-me');
